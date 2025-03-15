190085f662cc57401a188ee83ac29be9cc1ee4aa
anvil shakya <anvilsky123tg@gmail.com>
2025-03-15 20:52:20 +0545

    changed name and converted docx to pdf

---
+++ 04060823/project2/BSM_main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+#include "backend.c"
+
+int main(void)
+
+{
+
+    printf("\t-------MAIIN MENU-------\n\tWelcome to the Store\n");
+    int opt;
+
+    printf("\n\tYou Are:---\n\t1.Admin Login\n\t2.Customer\n");
+again:
+
+    printf("Login:  ");
+    scanf("%d", &opt);
+
+    switch (opt)
+    {
+    case 1:
+        adminlogin();
+        break;
+
+    case 2:
+        customer_menu();
+        break;
+
+    default:
+        printf("Invalid input\n");
+        system("cls");
+        goto again;
+        break;
+    }
+    return 0;
+}
---
+++ 04060823/project2/books.h
@@ -0,0 +1,13 @@
+
+#ifndef BOOKS_H
+#define BOOKS_H
+
+struct books {
+    char book[100];
+    char author[100];
+    char genre[100];
+    int price;
+    
+};
+
+#endif 
